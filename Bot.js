7605306a62aa2dbfd0c91a0d7c3f887cb5f92ab2
AngrySquid14 <>
2022-11-02 21:28:24 +0000

    Actual code is now in the file

--- Bot.js
+++ Bot.js
@@ -1,1 +1,20 @@
-//cock and balls
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'localhost',//change to the ip
+  port: 25565,// change to the port you want
+  username: 'Player', //change to the username of the account
+  password: 'password'//change to the password of the account
+})
+
+const bot = mineflayer.createBot(options)
+
+bot.once('spawn', () => {
+  bot.chat('hi!')
+})
+
+
+
+
+//this is tristian's phone number
+//206-8673452
