83017461108a8d62153ea5628db48c9d09ef7221
Jack Hembrow <jackwill8808@outlook.com>
2022-11-02 19:34:02 -0700

    Day 2

--- Bot.js
+++ Bot.js
@@ -1,20 +1,15 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: 'localhost',//change to the ip
-  port: 25565,// change to the port you want
-  username: 'Player', //change to the username of the account
-  password: 'password'//change to the password of the account
+  host: '147.135.105.184',//change to the ip
+  port: 25590,// change to the port you want
+  username: 'AngrySquid14', //change to the username of the account
+  password: undefined,//change to the password of the account
+  auth: 'microsoft'
 })
-
-const bot = mineflayer.createBot(options)
 
 bot.once('spawn', () => {
   bot.chat('hi!')
 })
 
 
-
-
-//this is tristian's phone number
-//206-8673452
