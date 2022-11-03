4bfbdb41e9e8c8f1502cc07c0dd83e01c9b3faf8
AngrySquid14 <110305204+AngrySquid14@users.noreply.github.com>
2022-11-03 22:38:48 +0000

    I FIXED THE CHANNEL ERROR!!

--- Bot.js
+++ Bot.js
@@ -1,15 +1,26 @@
 const mineflayer = require('mineflayer')
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
 
 const bot = mineflayer.createBot({
-  host: '147.135.105.184',//change to the ip
-  port: 25590,// change to the port you want
-  username: 'AngrySquid14', //change to the username of the account
+  host: '95.111.249.143',//change to the ip
+  port: 10000,// change to the port you want
+  username: 'Botofgaming', //change to the username of the account
   password: undefined,//change to the password of the account
-  auth: 'microsoft'
+  version: '1.19.2'
+ // auth: 'microsoft'
 })
 
 bot.once('spawn', () => {
   bot.chat('hi!')
+   mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+})
+
+bot.on('chat', (username, message) =>{
+  if (username === "AngrySquid14") {
+    if (message === 'attack me') {
+      bot.attack(username)
+    }
+  }
 })
 
 
